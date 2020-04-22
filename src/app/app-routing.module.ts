43bb6509be8c0770c9fea839357d9359fb50622e
Michael Mancini <thejazzymichael@gmail.com>
2020-04-22 16:02:03 -0400

    reverted module preloading change and changed meta tag image links to https

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -41,8 +41,7 @@ const routes: Routes = [
 ];
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes)
-    // RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
   ],
   exports: [RouterModule]
 })
