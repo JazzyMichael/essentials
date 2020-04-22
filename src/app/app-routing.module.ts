0c58cd3a3f316a21957f59d1fc830409defa3551
Michael Mancini <thejazzymichael@gmail.com>
2020-04-22 15:56:12 -0400

    added a feature graphic for seo thumbnail & test performance with no module preloading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -41,7 +41,8 @@ const routes: Routes = [
 ];
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes)
+    // RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
   ],
   exports: [RouterModule]
 })
