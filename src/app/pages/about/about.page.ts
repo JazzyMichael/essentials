e71b591842e78e8b57e4d59f54c02cf6f09f4bb8
Michael Mancini <thejazzymichael@gmail.com>
2020-05-13 15:55:41 -0400

    moved logout button to about page

--- src/app/pages/about/about.page.ts
+++ src/app/pages/about/about.page.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.page.html',
   styleUrls: ['./about.page.scss'],
 })
-export class AboutPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class AboutPage {
+  constructor(public auth: AuthService) { }
 }
